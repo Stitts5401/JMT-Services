8f1f84bfc523b934cb858b7498a81b43244be357
Jaden <jadenrs10@gmail.com>
2023-06-29 09:58:43 -0400

    Add sync communication between Order and Inventory service.

--- order-service/src/main/java/com/stitts/orderingservice/service/OrderService.java
+++ order-service/src/main/java/com/stitts/orderingservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.stitts.orderingservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +19,8 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -29,7 +32,19 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItemsList);
 
-        orderRepository.save(order);
+        // Call inventory service, and place order if product is in stock
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/v1/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(result){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
