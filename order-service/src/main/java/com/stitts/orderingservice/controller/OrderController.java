c047e5c507fb2e95318226a071c71ec96eaed29c
Jaden <jadenrs10@gmail.com>
2023-07-01 19:42:00 -0400

    Circuit breaker logic for failing InventoryService

--- order-service/src/main/java/com/stitts/orderingservice/controller/OrderController.java
+++ order-service/src/main/java/com/stitts/orderingservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.stitts.orderingservice.controller;
 
 import com.stitts.orderingservice.dto.OrderRequest;
 import com.stitts.orderingservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,14 @@ public class OrderController {
     private final OrderService orderservice;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name= "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderservice.placeOrder(orderRequest);
         return "Order placed successfully.";
     }
+
+    public String fallbackMethod(OrderRequest orderRequest , RuntimeException runtimeException){
+        return "Oops! Something went wrong, please order after some time! - \n" + runtimeException.getMessage();
+    }
+
 }
