aa4feb45dc647287f2a7174fce276b26b4181a9b
Jaden R Stittsworth <74199069+Stitts5401@users.noreply.github.com>
2023-12-16 13:46:30 -0500

    12/16

--- client-service/src/main/java/com/jmt/webservice/config/WebClientConfiguration.java
+++ client-service/src/main/java/com/jmt/webservice/config/WebClientConfiguration.java
@@ -1,27 +1,32 @@
 package com.jmt.webservice.config;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
-import org.thymeleaf.spring6.SpringTemplateEngine;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientConfiguration {
-  @Bean
-  @LoadBalanced
-  public WebClient.Builder webClient(
-      ReactiveClientRegistrationRepository clientRegistrationRepository,
-      ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
-    ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
-        new ServerOAuth2AuthorizedClientExchangeFilterFunction(
-            clientRegistrationRepository, authorizedClientRepository);
-    oauth.setDefaultOAuth2AuthorizedClient(true);
-    oauth.setDefaultClientRegistrationId("keycloak");
-    return WebClient.builder().filter(oauth);
-  }
+    @Bean
+    public WebClient.Builder webClient(ReactiveClientRegistrationRepository clientRegistrationRepository, ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
+        oauth.setDefaultOAuth2AuthorizedClient(true);
+        oauth.setDefaultClientRegistrationId("keycloak");
+
+        HttpClient httpClient = HttpClient.create()
+                .wiretap("reactor.netty.http.client.HttpClient",
+                        LogLevel.DEBUG,
+                        AdvancedByteBufFormat.TEXTUAL);
+
+        return WebClient.builder()
+                .filter(oauth)
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
+    }
 }
